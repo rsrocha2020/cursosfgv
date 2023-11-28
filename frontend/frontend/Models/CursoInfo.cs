075100ae8c8036ae5eb0f4de9b761a5c7e02e5d1
Rodrigo dos Santos Rocha <rodrigo.rocha@fgv.br>
2023-11-28 19:22:34 -0300

    Alterando frontend

--- frontend/frontend/Models/CursoInfo.cs
+++ frontend/frontend/Models/CursoInfo.cs
@@ -4,6 +4,6 @@ public class CursoInfo
 {
     public string? CursoNome { get; set; }
     public decimal CursoPreco { get; set; }
-    public string? Cursos { get; set; }
+    public string? Descricao { get; set; }
     public string? EmFalta { get; set; }
 }
